b5b6ddda3fa1e71b9e14f28aa3e956fdb46e1d67
luojunbin <luojunbin@bytedance.com>
2026-04-14 16:20:35 +0800

    Initial Commit

---
+++ bifrost-open-ios/bifrost_open_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  bifrost_open_iosApp.swift
+//  bifrost-open-ios
+//
+//  Created by ByteDance on 4/14/26.
+//
+
+import SwiftUI
+
+@main
+struct bifrost_open_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
